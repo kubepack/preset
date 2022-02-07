090909e7fcff4be3c044b20deeb93db5c4aec60b
Tamal Saha <tamal@appscode.com>
2022-02-06 23:38:05 -0800

    Add ChartPresetRef
    Signed-off-by: Tamal Saha <tamal@appscode.com>

--- apis/charts/v1alpha1/interface_types.go
+++ apis/charts/v1alpha1/interface_types.go
@@ -16,7 +16,12 @@ limitations under the License.
 
 package v1alpha1
 
-import "k8s.io/apimachinery/pkg/runtime/schema"
+import (
+	"fmt"
+	core "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime/schema"
+)
 
 //+kubebuilder:object:generate:=false
 type Preset interface {
@@ -36,4 +41,63 @@ var _ Preset = &ChartPreset{}
 
 func (in *ChartPreset) GetSpec() ClusterChartPresetSpec {
 	return in.Spec
+}
+
+type ChartPresetRef struct {
+	Name           string `json:"name"`
+	URL            string `json:"url"`
+	Version        string `json:"version"`
+	PresetGroup    string `json:"presetGroup,omitempty"`
+	PresetKind     string `json:"presetKind,omitempty"`
+	PresetName     string `json:"presetName,omitempty"`
+	PresetSelector string `json:"presetSelector,omitempty"`
+	Namespace      string `json:"namespace,omitempty"`
+}
+
+func (ref ChartPresetRef) ClusterChartPreset() (*ClusterChartPreset, error) {
+	if ref.PresetKind != ResourceKindVendorChartPreset &&
+		ref.PresetKind != ResourceKindClusterChartPreset {
+		return nil, fmt.Errorf("unknown preset kind %s", ref.PresetKind)
+	}
+
+	ps := ClusterChartPreset{
+		TypeMeta: metav1.TypeMeta{
+			APIVersion: GroupVersion.String(),
+			Kind:       ResourceKindClusterChartPreset,
+		},
+		ObjectMeta: metav1.ObjectMeta{
+			Name: "",
+		},
+		Spec: ClusterChartPresetSpec{
+			// select all
+			Selector: &metav1.LabelSelector{
+				MatchLabels:      map[string]string{},
+				MatchExpressions: nil,
+			},
+			// Values: nil,
+		},
+	}
+	if ref.PresetSelector != "" {
+		selector, err := metav1.ParseToLabelSelector(ref.PresetSelector)
+		if err != nil {
+			return nil, err
+		}
+		ps.Spec.Selector = selector
+	}
+
+	if ref.PresetName != "" {
+		group := ref.PresetGroup
+		if group == "" {
+			group = GroupVersion.Group
+		}
+		ps.Spec.UsePresets = []core.TypedLocalObjectReference{
+			{
+				APIGroup: &group,
+				Kind:     ref.PresetKind,
+				Name:     ref.PresetName,
+			},
+		}
+	}
+
+	return &ps, nil
 }
